aff012f67ad793ff75f322545f49cbedfd11b0fd
José Vieira da Silva Neto <josevieira2029@gmail.com>
2025-01-15 14:13:18 -0300

    Criando breakpoints globais para responsividade

---
+++ src/breakPoints.ts
@@ -0,0 +1,13 @@
+const size = {
+    mobile: '430',
+    tablet: '700',
+    desktop: '1010'
+}
+
+const breakpoints = {
+    mobile: `max-width: ${size.mobile}`,
+    tablet: `max-width: ${size.tablet}`,
+    desktop: `max-width: ${size.desktop}`,
+}
+
+export default breakpoints
