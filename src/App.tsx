b5583e0d9035709a7c47dd1c510ea0daac2e31dc
Joyce Santos <joyce069.js@gmail.com>
2025-02-10 16:10:39 -0300

    Criando tela de encontrar prestadores

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,17 @@ import {defaultTheme} from './styles/theme';
 import { Login } from "./pages/Login";
 import { Category } from "./pages/Category";
 import { Cadastro } from "./pages/Cadastro";
+import { EncontrarPrestador } from "./pages/EncontrarPrestadores";
 function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle/>
+      {/* <Home /> */}
+      <Login />
       <Home />
       {/* <Login /> */}
       {/* <Category category="Programador"/> */}
-      {/* <Home /> */}
-      {/* <EncontrarPrestador/> */}
-      <Cadastro/>
     </ThemeProvider>
   )
 }
---
+++ src/pages/EncontrarPrestadores/index.tsx
@@ -0,0 +1,73 @@
+import { CPCardPrestador } from "../../componentes/Cards/CPCardPrestador";
+import { CPFooter } from "../../componentes/Others/CPFooter";
+import { CPHeader1 } from "../../componentes/Others/CPHeader";
+import { DivCards, DivContainer, DivMedia, DivSubcontainer, DivTitulo, H1Titulo, PDescricao } from "./styled";
+
+
+export function EncontrarPrestador() {
+    return (
+        <DivContainer>
+            <DivSubcontainer>
+                <CPHeader1 iniciais="JV" name="José Vieira" variantType="secundario"></CPHeader1>
+                <DivTitulo>
+                    <H1Titulo>Encontrar Prestadores</H1Titulo>
+                    <PDescricao>Encontre abaixo todos os profissionais que você procura</PDescricao>
+                </DivTitulo>
+            </DivSubcontainer>
+            <DivMedia>
+                <DivCards>
+                    <CPCardPrestador
+                        city="São José de Piranhas - PB"
+                        email="jose@gmail.com"
+                        prestador="José Vieira"
+                        servico="Fisioterapeuta"
+                        telefone="(83) 99324-7454"
+                        uriFoto="fjghjfk"
+                    />
+                    <CPCardPrestador
+                        city="São José de Piranhas - PB"
+                        email="jose@gmail.com"
+                        prestador="José Vieira"
+                        servico="Fisioterapeuta"
+                        telefone="(83) 99324-7454"
+                        uriFoto="fjghjfk"
+                    />
+                    <CPCardPrestador
+                        city="São José de Piranhas - PB"
+                        email="jose@gmail.com"
+                        prestador="José Vieira"
+                        servico="Fisioterapeuta"
+                        telefone="(83) 99324-7454"
+                        uriFoto="fjghjfk"
+                    />
+                    <CPCardPrestador
+                        city="São José de Piranhas - PB"
+                        email="jose@gmail.com"
+                        prestador="José Vieira"
+                        servico="Fisioterapeuta"
+                        telefone="(83) 99324-7454"
+                        uriFoto="fjghjfk"
+                    />
+                    <CPCardPrestador
+                        city="São José de Piranhas - PB"
+                        email="jose@gmail.com"
+                        prestador="José Vieira"
+                        servico="Fisioterapeuta"
+                        telefone="(83) 99324-7454"
+                        uriFoto="fjghjfk"
+                    />
+                    <CPCardPrestador
+                        city="São José de Piranhas - PB"
+                        email="jose@gmail.com"
+                        prestador="José Vieira"
+                        servico="Fisioterapeuta"
+                        telefone="(83) 99324-7454"
+                        uriFoto="fjghjfk"
+                    />
+                </DivCards>
+                <CPFooter></CPFooter>
+            </DivMedia>
+        </DivContainer>
+    );
+
+}
