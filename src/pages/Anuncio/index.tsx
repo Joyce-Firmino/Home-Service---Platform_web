c5cbfd5e3f83f07d092fddc34e5f6555a79b702f
danrlei <dxnrleilira@gmail.com>
2025-03-12 11:37:51 -0300

    pequenas correções

--- src/pages/Anuncio/index.tsx
+++ src/pages/Anuncio/index.tsx
@@ -156,7 +156,7 @@ export function Anuncio() {
 
           <DivDireita>
             <CPButtonG
-              title="Criar anúncios"
+              title="Criar anúncio"
               variantType="primario"
               type="button"
               onClick={() => navigate("/criarAnuncio")}
